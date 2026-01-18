5e19f754ba477d051618903ba14e9fe71a80d680
balladaniel <96133731+balladaniel@users.noreply.github.com>
2026-01-18 20:55:40 +0100

    Added a language switcher (EN/HU), provided i18n strings for the UI. Message box (warnings/errors) strings are not translated yet, they are HU only. ORS route requests use the UI language, so itinerary has appropriate instructions in the current language.

---
+++ translations.js
@@ -0,0 +1,152 @@
+var translations = {
+    en: {
+        // Hover and main route points panels:
+        waypoint1Input: ["Starting point", "Input a location, a coordinate pair in latitude, longitude format (e.g. 47.4511, 19.4577) or click here and then on the map"],
+        waypointMidInput: ["Travel through", "Input a location, a coordinate pair in latitude, longitude format (e.g. 47.4511, 19.4577) or click here and then on the map"],
+        waypoint2Input: ["Destination", "Input a location, a coordinate pair in latitude, longitude format (e.g. 47.4511, 19.4577) or click here and then on the map"],
+        isochronePointInput: ["Starting point", "Input a location, a coordinate pair in latitude, longitude format (e.g. 47.4511, 19.4577) or click here and then on the map"],
+        geolocate: "Use your location",
+        deleteMidPoint: "Remove destination",
+        reverseRouteDirection: "Reverse route points",
+        goButton: "Go",
+        otherParams: "Refine route options >",
+        quicklinkForIsochroneMode: "Isochrone generation >",
+
+        // Sidebar main tabs
+        menuTabRoutingBtn: "Route Planner",
+        menuTabIsochroneBtn: "Isochrone Generation",
+
+        // Route / Planning parameters
+        avoidTitleLabel: "Avoid:",
+        avoidHighways: "Highways",
+        avoidTollways: "Tollways",
+        avoidFerries: "Ferries",
+        avoidCountryBorders: "Country borders",
+        routePreferenceLabel: "Preference:",
+        routePreferenceRecommended: "Recommended",
+        routePreferenceShortest: "Shortest",
+        routePreferenceFastest: "Fastest",
+
+        // Route / General information
+        routeInfoTimeLabel: "Time:",
+        routeInfoDistanceLabel: "Distance:",
+        
+        // Route / Itinerary section (collapsible)
+        itinerarySectionHeader: "Itinerary",
+        itineraryTableHeaderInstructionLabel: "Instruction",
+        itineraryTableHeaderDistanceLabel: "Distance",
+        itineraryTableHeaderTimeLabel: "Time",
+        itineraryTableEmpty: "No route found",
+
+        // Route / Elevation profile section (collapsible)
+        elevationProfileSectionLabel: "Elevation profile",
+        elevationProfileChartDatasetLabel: "Elevation (m)",
+
+        // Isochrone / Generation parameters
+        isochroneOptionsLabel: "Options:",
+        isochroneOptionMethodLabel: "Method (time/distance):",
+        isochroneOptionMaxTimeLabel: "Maximum time:",
+        isochroneOptionMaxDistanceLabel: "Maximum distance:",
+        isochroneOptionIsochCountLabel: "Number of isochrones:",
+        isochroneOptionMethodTime: "Time",
+        isochroneOptionMethodDistance: "Distance",
+
+        // Isochrone / Results section (collapsible)
+        isochroneResultSectionHeader: "Results",
+        isochroneResultSectionTableHeaderDistance: "Distance",
+        isochroneResultSectionTableNoResults: "No results",
+
+        // Isochrone / Map
+        isochronePolygonTooltipTimeLabel: "Time",
+        isochronePolygonTooltipDistanceLabel: "Distance",
+
+        // Profile types
+        profileCar: "By car (driving)",
+        profileBike: "By bike (cycling)",
+        profileWalk: "By foot (walking)",
+
+        // Misc functions
+        exportButton: "Export GeoJSON",
+
+        // Attribution section
+        attributionProcessIsochronegenLabel: "Isochrone generation",
+        attributionProcessRouteplanningLabel: "Route planning",
+        attributionProcessElevationdataLabel: "Elevation data",
+        attributionORSTimestamp: "Request timestamp",
+    },
+    hu: {
+        // Hover and main route points panels:
+        waypoint1Input: ["Kiindulási pont", "Adjon meg egy helyet, egy koordinátapárt szélesség, hosszúság sorrendben (pl. 47.4511, 19.4577) vagy kattintson ide, majd a térképre"],
+        waypointMidInput: ["Közbeeső pont", "Adjon meg egy helyet, egy koordinátapárt szélesség, hosszúság sorrendben (pl. 47.4511, 19.4577) vagy kattintson ide, majd a térképre"],
+        waypoint2Input: ["Célpont", "Adjon meg egy helyet, egy koordinátapárt szélesség, hosszúság sorrendben (pl. 47.4511, 19.4577) vagy kattintson ide, majd a térképre"],
+        isochronePointInput: ["Középpont", "Adjon meg egy helyet, egy koordinátapárt szélesség, hosszúság sorrendben (pl. 47.4511, 19.4577) vagy kattintson ide, majd a térképre"],
+        geolocate: "Helymeghatározás",
+        deleteMidPoint: "Közbeeső útvonalpont törlése",
+        reverseRouteDirection: "Irány megfordítása",
+        goButton: "Go",
+        otherParams: "Egyéb keresési paraméterek >",
+        quicklinkForIsochroneMode: "Izokrónkészítés >",
+        
+        // Sidebar main tabs
+        menuTabRoutingBtn: "Útvonaltervezés",
+        menuTabIsochroneBtn: "Izokrónkészítés",
+
+        // Route / Planning parameters
+        avoidTitleLabel: "Elkerülendő:",
+        avoidHighways: "Autópályák",
+        avoidTollways: "Útdíjak",
+        avoidFerries: "Kompok",
+        avoidCountryBorders: "Országhatárok",
+        routePreferenceLabel: "Útvonal:",
+        routePreferenceRecommended: "Ajánlott",
+        routePreferenceShortest: "Legrövidebb",
+        routePreferenceFastest: "Leggyorsabb",
+
+        // Route / General information
+        routeInfoTimeLabel: "Idő:",
+        routeInfoDistanceLabel: "Hossz:",
+
+        // Route / Itinerary section (collapsible)
+        itinerarySectionHeader: "Itiner",
+        itineraryTableHeaderInstructionLabel: "Instrukció",
+        itineraryTableHeaderDistanceLabel: "Hossz",
+        itineraryTableHeaderTimeLabel: "Idő",
+        itineraryTableEmpty: "Nincs útvonal",
+
+        // Route / Elevation profile section (collapsible)
+        elevationProfileSectionLabel: "Magassági profil",
+        elevationProfileChartDatasetLabel: "Magasság (m)",
+
+        // Isochrone / Generation parameters
+        isochroneOptionsLabel: "Beállítások:",
+        isochroneOptionMethodLabel: "Módszer (idő/távolság):",
+        isochroneOptionMaxTimeLabel: "Maximális idő:",
+        isochroneOptionMaxDistanceLabel: "Maximális táv:",
+        isochroneOptionIsochCountLabel: "Izokrónok száma (db):",
+        isochroneOptionMethodTime: "Izokrón",
+        isochroneOptionMethodDistance: "Izodisztans",
+
+        // Isochrone / Results section (collapsible)
+        isochroneResultSectionHeader: "Eredmények",
+        isochroneResultSectionTableHeaderDistance: "Távolság",
+        isochroneResultSectionTableNoResults: "Nincs eredmény",
+
+        // Isochrone / Map
+        isochronePolygonTooltipTimeLabel: "Idő",
+        isochronePolygonTooltipDistanceLabel: "Távolság",
+
+        // Profile types
+        profileCar: "Autóval",
+        profileBike: "Biciklivel",
+        profileWalk: "Gyalog",
+
+        // Misc functions
+        exportButton: "Export GeoJSON",
+
+        // Attribution section
+        attributionProcessIsochronegenLabel: "Izokróngenerálás",
+        attributionProcessRouteplanningLabel: "Útvonaltervezés",
+        attributionProcessElevationdataLabel: "Magassági adatok",
+        attributionORSTimestamp: "Kérés ideje",
+    }
+}
